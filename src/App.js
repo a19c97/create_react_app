d8bf7b8621303bc5844679ea6b4abbe000057e7e
Harley Vanselow <harleyvanselow@gmail.com>
2020-06-14 18:19:49 -0400

    Board yet?

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Chessboard } from 'chessboardjs';
-// var chessboardjs = require('@chrisoakman/chessboardjs');
+import ChessboardJs from 'react-chessboardjs-wrapper'
 
+export default props => <ChessboardJs {...props} />
 class App extends React.Component{
   render() {
-    var board = Chessboard('myBoard');
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Salutations!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ChessboardJs
+        animate // boolean, chessboard.js piece animations
+        blackSquareColour="steelblue" // or hex
+        border="10px solid #d3d3d3" // css border property
+        onInitBoard={(board, boardId) => this._board = board} // callback fn, gets passed the chessboard.js board object, and the unique id
+        resize // effective if width prop is a string
+        whiteSquareColour="aliceblue" // or hex
+        width="80%" // string (%) || number (px)
+      />    
     );
   }
 }
-
-export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Salutations!!!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
